43cbbec348f125d34d52589eade3d1a6282d5c49
monsterpdk <toth.arpad@pdk.hu>
2016-08-18 02:37:41 +0200

    sdk konyvtar eltavolitasa

--- Model/Sdk/exports.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @Deprecated
- */
-function purchasedat_render_script($apiKey, $purchaseOptions, $target = null, $jwtOptions = null)
-{
-    return \PurchasedAt\PurchaseScript::render($apiKey, $purchaseOptions, $target, $jwtOptions);
-}
