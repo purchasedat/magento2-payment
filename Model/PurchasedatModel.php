35257378aefa135fc6b4243eab6d75b719460e2f
monsterpdk <toth.arpad@pdk.hu>
2016-09-15 15:25:44 +0200

    penznem anomalia javitas meg

--- Model/PurchasedatModel.php
+++ Model/PurchasedatModel.php
@@ -240,7 +240,6 @@ class PurchasedatModel extends \Magento\Payment\Model\Method\AbstractMethod
         if ($quote->getGrandTotal() && ($customer->getEmail() || $guest_email != "")) {
             $quote_data = $quote->getData();
             $grand_total = $quote_data['grand_total'];
-            $subtotal = $quote_data['subtotal_with_discount'];
             $api_key = $this->getConfigData('api_key');
             if ($customer->getEmail()) {
                 $customer_email = $customer->getEmail() ;
@@ -261,7 +260,6 @@ class PurchasedatModel extends \Magento\Payment\Model\Method\AbstractMethod
             $currency_code = $this->_storemanager->getStore()->getCurrentCurrency()->getCode() ;
             $checkout = null;
 
-//            $shipping_rate = $grand_total - $subtotal;
             $shipping_rate = $quote_data['shipping_amount'] ;
             // Create items list
             foreach ($this->_session->getQuote()->getAllItems() as $items) {
