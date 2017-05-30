138c7f7c8ec2e7ce27fab31b0958d8bd11128dc9
joe <joe@chilicreative.hu>
2017-05-31 00:43:43 +0200

    Merge upstream modifications from session-refact

--- Block/Payment/Finish.php
+++ Block/Payment/Finish.php
@@ -137,8 +137,8 @@ class Finish extends \Magento\Framework\View\Element\Template
     public function getSessionData($key, $clear = false)
     {
         $om = \Magento\Framework\App\ObjectManager::getInstance();
-        $catalogSession = $om->create('\Magento\Catalog\Model\Session');
-        return $catalogSession->getData($key, $clear);
+        $checkoutSession = $om->create('\Magento\Checkout\Model\Session');
+        return $checkoutSession->getData($key, $clear);
     }
 
     /**
